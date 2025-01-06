fe32087f72ae7d23a0cdbc6fc6687ab72fae2eec
Thoriso Molebatsi <Thoriso.Molebatsi@ilabqa.com>
2025-01-06 12:17:46 +0200

    Learning objectives for chapter 1 completed. close #1

--- Java_A_Training/src/Chapter1/UsingConstants.java
+++ Java_A_Training/src/Chapter1/UsingConstants.java
@@ -2,10 +2,29 @@ package Chapter1;
 
 public class UsingConstants {
     public static void main(String[] args) {
+        //DECLARE
         int age;
         int birthYear;
+        double salary;
+        int hoursWorked;
+        String name;
         final int CURRENT_YEAR = 2025;
         final String COMPANY = "ILAB Quality";
+        final double HOURLY_RATE = 350;
+        final double TAX = 0.25;
+
+        //ASSIGN
+        name = "James";
+        birthYear = 1994;
+        age =CURRENT_YEAR - birthYear;
+        hoursWorked = 200;
+        salary = (hoursWorked * HOURLY_RATE) - ((hoursWorked * HOURLY_RATE) * TAX);
+
+        //USE
+        System.out.println("==================="+ COMPANY +"=====================");
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
 
     }
 }
