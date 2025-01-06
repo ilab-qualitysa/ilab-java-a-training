6b8be182bba777c0b373dae2f650d044892100ce
Thoriso Molebatsi <Thoriso.Molebatsi@ilabqa.com>
2025-01-06 11:15:54 +0200

    program created using different data types #1

---
+++ Java_A_Training/src/Chapter1/GetStarted.java
@@ -0,0 +1,31 @@
+package Chapter1;
+
+public class GetStarted {
+    public static void main(String[] args) {
+        System.out.println("Welcome to java training");
+
+        int age;
+        double height;
+        float salary;
+        boolean isContractor;
+        String name;
+        char positionCode; // S-senior, J-Junior, I-Intern
+
+
+        //Assign
+        age = 30;
+        height= 1.89;
+        salary= 23000.358F;
+        isContractor = true;
+        name = "Johnny";
+        positionCode = 'J';
+
+        //Use
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Salary: " + salary);
+        System.out.println("Position Code: " + positionCode);
+
+    }
+}
