28a625cfa56424e68e1578bfb72c41992e40feaa
Thoriso Molebatsi <Thoriso.Molebatsi@ilabqa.com>
2025-01-09 15:46:13 +0200

    files added

---
+++ Java_A_Training/src/Chapter6/DebugExercise4.java
@@ -0,0 +1,9 @@
+package Chapter6;
+public class DebugExercise4 {
+   public static void main(String[] args) {         
+  double x = 5.0;
+  int y = 2;         
+  double result = x / y;
+  System.out.println("Result: " + result);     
+  } 
+}
---
+++ Java_A_Training/src/Chapter6/DebugExercise7.java
@@ -0,0 +1,11 @@
+package Chapter6;
+public class DebugExercise7 {
+   public static void main(String[] args) {         
+     printMessage("Hello, World!");     
+ }   
+   
+private static String printMessage(String message) {
+    System.out.println("Message: " + message);         
+    return message;     
+  }  
+}
