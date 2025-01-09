e43958649872b994ef995a99ce402cee1710c5ff
Thoriso Molebatsi <Thoriso.Molebatsi@ilabqa.com>
2025-01-09 08:46:56 +0200

    done with chapter 4. close #9

--- Java_A_Training/src/Chapter4/GenericMethods.java
+++ Java_A_Training/src/Chapter4/GenericMethods.java
@@ -2,13 +2,22 @@ package Chapter4;
 
 public class GenericMethods {
     public static void main(String[] args) {
+        int l = 0;
 
         System.out.println(calcSum(2, 2));
-
+        GenericMethods.<Integer>displayNumber(4,3);
+        GenericMethods.<Long>displayNumber(4L,2L);
+        checkNameLength("uvuvuy", 8);
+    }
+    static <T> void displayNumber(T x, T y){
+        System.out.println("x = " + x + ", y = " + y);
     }
     static <T extends Number> double calcSum(T num1, T num2){
         return num1.doubleValue() + num2.doubleValue();
     }
-
+    static <T1, T2> void checkNameLength(T1 strName, T2 intLen){
+        System.out.println(strName + " has " + intLen + " characters. " +
+                "(" + (intLen.equals(strName.toString().length())) + ")");
+    }
 
 }
